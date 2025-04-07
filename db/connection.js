931da319b68e19494944166ee6c3af641e981f05
Albert <albertoma24@gmail.com>
2025-04-07 08:14:56 -0700

    more debugging

--- db/connection.js
+++ db/connection.js
@@ -3,9 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    
-  })
+  .connect(process.env.MONGODB_URI, )
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
