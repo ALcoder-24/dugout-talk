1777b6bf7bee89011c774caa05bf5d208e3caed6
Albert <albertoma24@gmail.com>
2025-04-07 08:09:19 -0700

    more debugging link

--- db/connection.js
+++ db/connection.js
@@ -4,8 +4,7 @@ dotenv.config();
 
 mongoose
   .connect(process.env.MONGODB_URI, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
+    
   })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
